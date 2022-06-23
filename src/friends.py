b3c309f29238cdb1db045ad216028cfb75d68e75
Lewis <lewisdsykes@gmail.com>
2022-06-23 12:48:45 +0100

    first commit

---
+++ src/friends.py
@@ -0,0 +1,52 @@
+def get_name(person):
+    return person["name"]
+
+def get_favourite_tv_show(person):
+    return person["favourites"]["tv_show"]
+
+def likes_to_eat(person, food):
+    for snack in person["favourites"]["snacks"]:
+        if snack == food:
+            return True
+
+    return False
+
+def add_friend(person, friend):
+    person["friends"].append(friend)
+
+def remove_friend(person, friend):
+    person["friends"].remove(friend)
+
+def total_money(people):
+    total_money = 0
+    for person in people:
+       total_money += person["monies"] 
+    return total_money 
+
+
+
+        
+
+        
+    
+        
+
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
